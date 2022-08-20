8ff2495053538d61ad5c0b275598ea28082112d6
CephasTI <109244573+CephasTI@users.noreply.github.com>
2022-08-20 02:46:13 -0300

    Mudando fonte da letra e linkando com o js

---
+++ conta.js
@@ -0,0 +1,30 @@
+let calculador = document.querySelector("#calculador");
+let valor = document.querySelector("#valor");
+let venda = document.querySelector("#venda");
+let retomada = document.querySelector("#retomada")
+
+let quantValor = valor.value;
+let quantVenda = venda.value;
+
+let lucro = quantVenda - quantValor;
+let porcentagem = Math.round(lucro / quantValor * 100);
+
+function mostrar(mostra){
+    document.write(mostra)
+}
+
+if(isNaN(porcentagem)) {
+    mostrar(0 + "%");
+}else{
+    mostrar(porcentagem + "%")
+}
+
+calculador.addEventListener("click", function() {
+    
+    location.reload();
+
+});
+
+//calculador.onclick = document.location.replace; 
+
+console.log(porcentagem);
